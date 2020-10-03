4d98150f045c06c7c70c7acac98a15b0dbabb776
Nikrybkin <71150246+Nikrybkin@users.noreply.github.com>
2020-10-03 16:55:09 +0400

    Моя программка, для решения квадратных уравнений

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,36 @@
-#include <iostream>               // подключаем заголовочный файл iostream
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
 
-int main()                          // определяем функцию main
-{                                   // начало функции
-	std::cout << "Hello World!";  // выводим строку на консоль
-	return 0;                       // выходим из функции
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+		cout << "Введите уравнение a b c через пробел :";
+	int a, b, c;
+	cin >> a >> b >> c;
+	double d = b * b - 4 * a * c;
+	cout << "d=" << d << endl;
+
+	if (d > 0)
+	{
+		double x1 = (-b - d) / 2 / a;
+		double x2 = (-b + d) / 2 / a;
+		cout << "x1=" << x1 << endl;
+		cout << "x2=" << x2 << endl;
+	}
+	else if (d = 0)
+	{
+		double x = (-b) / 2 / a;
+		cout << "x=" << x << endl;
+	}
+	if (d < 0)
+	{
+		cout << "Нет решений" << endl;
+	}
+	_getch();
+	return 0;
 }
