f75b9987da1cff89afea0863fcefb4e62a7085f3
Alexey <37597550+Borshecs@users.noreply.github.com>
2020-10-13 21:05:03 +0300

    выводить квадрат или фигру по данным польз

---
+++ квадрат по данным пользователя.cpp
@@ -0,0 +1,24 @@
+﻿#include "cstdio"
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	std::printf("Введите размер");
+	std::printf("длины и ширины через пробел");
+	cin >> a >> b;
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			if ((i == 0) or (j == 0) or (i == a - 1) or (j == b - 1)) cout << "|";
+			else cout << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+
+
+}
