fd9080d35217ee08524562020661e68413ef4f27
b1hh1 <71234596+b1hh1@users.noreply.github.com>
2020-10-18 02:33:30 +0300

    Update kvadrat.cpp

--- kvadrat.cpp
+++ kvadrat.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int a;
-    cin >> a;
-    for (int i = 1; i < a+1; i++){
-        for (int j = 1; j < a+1; j++){
-        if ((i == 1) or (j == 1) or (i == a) or (j == a))
+    for (int i = 1; i < 9; i++){
+        for (int j = 1; j < 9; j++){
+        if ((i == 1) or (j == 1) or (i == 8) or (j == 8))
         {
             cout << "##";
         }
