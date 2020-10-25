3f0fba32bf0b24e4ec114d94ab093f084a136944
Kislota1 <70697570+Kislota1@users.noreply.github.com>
2020-10-25 15:43:40 +0300

    Add files via upload
    pr9moygol'nik

---
+++ pr9moygol'nik.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+int main() {
+  
+    int x, y, i, j;
+    cout << "Shiriny pr9moygol'nika: ";
+    cin >> x;
+    cout << "Visoty pr9moygol'nika: ";
+    cin >> y;
+    for (i = 1; i <= x; i++)
+        cout << "*";
+    cout << endl;
+    for (j = 1; j <= y - 2; j++) {
+        cout << "*";
+        for (i = 1; i <= x - 2; i++)
+            cout << " ";
+        cout << "*" << endl;
+    }
+    for (i = 1; i <= x; i++)
+        cout << "*";
+    
+    return 0;
+}
+    
