8dfbb3edb79362176a148bd797d02b90e160eca0
Elllen <71223333+LenaNaumova@users.noreply.github.com>
2020-11-03 17:12:53 +0300

    dodelt

---
+++ dzkonstantinov2.0/dzkonstantinov2.0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Calc.h"
+#include <string>
+
+
+
+int main(int argc, char* argv[])
+{
+	double a = 2.0;
+	double b = 4.1;
+	double xn = 0.77;
+	double xk = 1.77;
+	double dx = 0.2;
+
+	taskA(a, b, xn, xk, dx);
+
+	return 0;
+}
+
