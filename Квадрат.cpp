8de793795d915d067f2dd9f0bc0347566ca188ff
Dasfor <71176297+Dasfor@users.noreply.github.com>
2020-11-03 20:29:54 +0300

    Add files via upload

---
+++ Квадрат.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+	int i; cout << "Enter size: "; cin >> i;
+	for (int x = 0;x < i;x++)
+	{
+		for (int y = 0;y < i;y++)
+		{
+			if ((x == 0) || (x == (i - 1)) || (y == 0) || (y == (i - 1)))
+			{
+				cout << "||";
+			}
+			else
+			{
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ Пример.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+double func(double x = 0)
+{
+	double y = pow(abs(x * x - 2.5), 1.0 / 4.0) + pow(log(x * x), 1.0 / 3.0);
+	return y;
+}
+int main()
+{
+	cout << "Example: " << endl;
+	cout << endl;
+	cout << "Task A: " << endl;
+	double ResA[50] = { 0 };
+	double xn = 1.25;
+	double xk = 3.25;
+	double dx = 0.4;
+	int i = 0;
+	while (xn <= xk)
+	{
+		cout << xn << " => ";
+		*(ResA + i) = func(xn);
+		i++;
+		xn = xn + dx;
+		cout << func(xn) << endl;
+	}
+
+	cout << endl;
+
+	cout << "Task B: " << endl;
+	const int n = 5;
+	double task_B[n] = { 1.84, 2.71, 3.81, 4.56, 5.62 };
+	double ResB[n] = { 0, 0, 0, 0, 0 };
+	for (int i = 0; i != n; )
+	{
+		cout << (*(task_B + i)) << " => ";
+		*(ResB + i) = func(*(task_B + i));
+		i++;
+		cout << func(*(task_B + i)) << endl;
+	}
+}
