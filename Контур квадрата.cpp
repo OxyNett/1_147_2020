1dac56ff56f4314f7564417b275cf67298a2c7c3
Greenprize <baranka2003@yandex.ru>
2020-11-03 19:03:41 +0300

    labvork

--- Контур квадрата.cpp
+++ Контур квадрата.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() 
+int main()
 {
-  for(int k=1; k<11; k++)
-  {
-    for(int n=1; n<10; n++)
+    for (int k = 1; k < 11; k++)
     {
-      if((k==1) or (n==1) or (k==10) or (n==10))
-      {
-        cout << "~";
-      }
-      else
-      {
-        cout << "  ";
-      }
+        for (int n = 1; n < 10; n++)
+        {
+            if ((k == 1) or (n == 1) or (k == 10) or (n == 10))
+            {
+                cout << "~";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  return 0;
-} 
+    return 0;
+}
