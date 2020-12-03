ce8c5f3ec0aa20a77672c9f10537ea830a1d7b2f
Kislota1 <70697570+Kislota1@users.noreply.github.com>
2020-12-04 02:29:42 +0300

    Add files via upload

---
+++ Classes.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Human
+{
+
+private:
+
+    string name;
+    int year;
+    float body_weight;
+
+public:
+    void display() {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << year << endl;
+        cout << "Weight: " << body_weight << endl;
+    }
+
+    void changeName(string name) {
+        this->name = name;
+    }
+    Human(string name, int year, float body_weight)
+    {
+        this->name = name;
+        this->year = year;
+        this->body_weight = body_weight;
+    }
+};
+
+int main()
+{
+    Human* obj_Human = new Human("Vasya", 12, 80);
+    obj_Human->display();
+    obj_Human->changeName("Ivan");
+    obj_Human->display();
+
+    return 0;
+}
