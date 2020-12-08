3c1a70dcb0d9675380ca6b298a3f26ad4cdae5ba
Greenprize <baranka2003@yandex.ru>
2020-12-08 19:39:02 +0300

    Oksana Sergeevna Netkacheva

---
+++ Changes.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+class Person
+{
+private:
+
+     double avermark;
+
+public:
+
+    string name;
+    int age;
+
+    Person(string n, int a, double aver)
+    {
+        name = n; 
+        age = a; 
+        avermark = aver;
+    }
+
+    void changemark(double avermark)
+    {
+        if (avermark >= 0)
+            this->avermark = avermark;
+    }
+
+    void move()
+    {
+        cout << name << " is moving" << endl;
+    }
+
+    void display()
+    {
+        cout << '\n' << "Name: " << name << "\tAge: " << age << "\tAverage mark: " << endl;
+    }
+};
+int main()
+{
+    Person person = Person("Tom", 22, 3.5);
+    person.changemark(-4);
+    person.display();
+    person.move();
+
+    return 0;
+}
